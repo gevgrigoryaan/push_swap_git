e61b3e00eda05e307707b605842b19380c2af2bc
Gev Grigoryan <gevgrigoryaan@gmail.com>
2026-02-24 20:04:47 +0400

    s

--- operation_r_rotate.c
+++ operation_r_rotate.c
@@ -6,7 +6,7 @@
 /*   By: gegrigor <gevgrigoryaan@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 18:33:23 by gegrigor          #+#    #+#             */
-/*   Updated: 2026/02/24 17:03:19 by gegrigor         ###   ########.fr       */
+/*   Updated: 2026/02/24 19:57:15 by gegrigor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ static void    r_rotate(t_stack **head)
     t_stack *new_tail;
 
     if (!(*head) || !((*head)->next))
+        return ;
     tail = (*head);
     while (tail->next)
     {
@@ -32,17 +33,17 @@ static void    r_rotate(t_stack **head)
 void    r_rotate_a(t_stack **a)
 {
     r_rotate(a);
-    write(1, "rra\n", 3);
+    write(1, "rra\n", 4);
 }
 
 void    r_rotate_b(t_stack **b)
 {
     r_rotate(b);
-    write(1, "rrb\n", 3);
+    write(1, "rrb\n", 4);
 }
 void    r_rotate_ab(t_stack **a, t_stack **b)
 {
     r_rotate(a);
     r_rotate(b);
-    write(1, "rrr\n", 3);
+    write(1, "rrr\n", 4);
 }
